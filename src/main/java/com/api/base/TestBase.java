c6d45f83871757f3e23f73ab221e7697ab4545cf
Manoj Hans <manoj.hans@macys.com>
2019-03-20 23:47:24 +0530

    Updated resource folder location

--- src/main/java/com/api/base/TestBase.java
+++ src/main/java/com/api/base/TestBase.java
@@ -1,34 +1,23 @@
 package com.api.base;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Random;
 
 public class TestBase {
-	
-	protected int RESPONSE_STATUS_CODE_200 = 200;
-	protected int RESPONSE_STATUS_CODE_500 = 500;
-	protected int RESPONSE_STATUS_CODE_400 = 400;
-	protected int RESPONSE_STATUS_CODE_404 = 404;
-	protected int RESPONSE_STATUS_CODE_401 = 401;
-	protected int RESPONSE_STATUS_CODE_201 = 201;
 
 	protected Properties prop;
 	
-	public TestBase(){
-		try {
+	public TestBase() {
+        try {
 			prop = new Properties();
-			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/config.properties");
-			prop.load(ip);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+			FileInputStream ip = new FileInputStream(this.getClass().getResource("/config.properties").getFile());
+            prop.load(ip);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 	
 	public String randomNumber() {
 		Random random =new Random();
